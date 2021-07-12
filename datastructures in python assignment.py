cde2b687503e9859f44561f789ce1f13382226af
SomyaRanjanMohapatra <62986681+SomyaRanjanMohapatra@users.noreply.github.com>
2021-07-12 22:03:03 +0530

    Add files via upload

---
+++ datastructures in python assignment.py
@@ -0,0 +1,68 @@
+#if statement in python
+flag= True
+if  flag==True:
+    print("welcome to python")
+    
+num=100
+if num<200:
+    print ("num is less than 200")
+    
+# python for loop
+numbers =[1,2,3,4,5,6]
+sum=0
+for val in numbers:
+    sum=sum+val
+print("the sum is ",sum )
+# the range() function
+print(range(10))
+print(list(range(2,8)))
+#data structures in python
+
+
+list=["potato","onion","garlic","brinjal"]
+>>> list[0]
+'potato'
+>>> list[3]
+'brinjal'
+>>> print(list)
+['potato', 'onion', 'garlic', 'brinjal']
+>>> lists are used to store multiple items in a single variable
+SyntaxError: invalid syntax
+>>> sets methods
+SyntaxError: invalid syntax
+>>> #sets methods
+>>> set={"bulb","fridge","washing machine"}
+>>> print(type(set))
+<class 'set'>
+>>> set[0]
+TypeError: 'set' object is not subscriptable
+>>> set{0}
+SyntaxError: invalid syntax
+>>> #this shows set is unordered in nature
+>>> #and a set cannot be modified.
+>>> my_dict={'name':'jack','age':26}
+>>> print(my_dict['name'])
+jack
+>>> #dictionaries can be changed i.e are mutable
+>>> my_dict['age']=27
+>>> print(my_dict)
+{'name': 'jack', 'age': 27}
+>>> print(my_dict)
+{'name': 'jack', 'age': 27}
+>>> #add item
+>>> my_dict['ADRESS']='Downtown'
+>>> print(my_dict)
+{'name': 'jack', 'age': 27, 'ADRESS': 'Downtown'}
+>>> #string codes
+>>> my_string='hello'
+>>> print(my_string)
+hello
+>>> print('my_string =',my_string[0])
+my_string = h
+>>> print('my_string =',my_string[-1])
+my_string = o
+>>>  print('my_string[1:5]',my_string[-1])
+ 
+SyntaxError: unexpected indent
+>>>  print('my_string[1:5] =',my_string[-1])
+ 
